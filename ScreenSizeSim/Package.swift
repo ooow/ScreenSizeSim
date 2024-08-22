057263f427491df0222f2107a6decd134d6ceea6
Goga <tirkiya@icloud.com>
2024-08-22 21:14:02 +0200

    Add package information.

---
+++ ScreenSizeSim/Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+  name: "ScreenSizeSim",
+  platforms: [
+    .iOS(.v15),
+  ],
+  products: [
+    .library(
+      name: "ScreenSizeSim",
+      targets: ["ScreenSizeSim"]),
+  ],
+  targets: [
+    .target(
+      name: "ScreenSizeSim",
+      dependencies: []),
+    .testTarget(
+      name: "ScreenSizeSimTests",
+      dependencies: ["ScreenSizeSim"]),
+  ]
+)
