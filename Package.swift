77d319dba64211e08becf9682945bc01b2121de5
Goga <tirkiya@icloud.com>
2024-08-22 22:08:27 +0200

    Correct typo in package info.

--- Package.swift
+++ Package.swift
@@ -14,6 +14,6 @@ let package = Package(
       targets: ["ScreenSizeSim"]),
   ],
   targets: [
-    .target(name: "ScreenSizeSim", dependencies=[]),
+    .target(name: "ScreenSizeSim", dependencies: []),
   ]
 )
